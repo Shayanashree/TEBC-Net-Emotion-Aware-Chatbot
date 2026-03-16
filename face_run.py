3023d40c0fd03950c98bd47341384a1b09498f34
Shayanashree <shayanashree04@gmail.com>
2026-03-16 11:26:02 +0530

    Comment out model_from_json import
    Comment out the import statement for model_from_json.

--- face_run.py
+++ face_run.py
@@ -3,7 +3,7 @@ import logging, os
 # logging.disable(logging.WARNING)
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-from keras.models import model_from_json
+#from keras.models import model_from_json
 import numpy as np
 
 
